c237d93443f813876e728c1d34efe4e8dd565972
heysushil <sushil.chaudhary111@gmail.com>
2021-06-20 16:50:31 +0530

    check roll number condition if dublicate then ask for unique roll number

--- pedding_works.md
+++ pedding_works.md
@@ -2,5 +2,10 @@
 
 1. Choose_class fucniton ke bad me jaise hi new class create hua then again new choose_class fucntion ko show karna hai. Taki admin ayse hi aur classes bana sake ek bar me hi.
 1. student_options_in_class is fucntion ke andar hum admin ko multipal options show karenge.
-
-## How to validate roll number when registering new student
+1. How to validate roll number when registering new student
+1. new student registration ke bad fir se student roll number mang raha hai. isko kaise solve karen?
+1. New registration hone ke bad success message dikhane ke bad kaise dubara ek class se realated option show karaya.
+1. ab ek class ke existing sudent ka data roll number ke behalf par kaise dikhaye.
+1. ek student ki detail dekhne ke bad dubara ek class ke baki option kaise dikhayen.
+1. ab ek class ke student ki koi detial change ya update karni hai kaise karen?
+1. 
--- students_related_work.py
+++ students_related_work.py
@@ -5,25 +5,30 @@ def check_roll_number(school_name, class_name, roll_number):
     # student_details name ki file ko read mode me open kiya, taki is file me se roll number get kiya ja sake.
         get_studnet_details = open('schools/'+school_name+'/'+class_name+'/student_details.txt', 'r')
         get_roll_number = get_studnet_details.readlines()
-        # get_individual_rn = []
-        # str to dictinary conversion ke liye json ka use karenge.
+
+        # loop me individula roll number ko get karna hai
+        get_individual_rn = []
         for r in get_roll_number:
-            # print('type: ', type(json.loads(r)))
-            e_rn = json.loads(r) # e_rn = existing roll number
-            # print(e_rn)
-            existing_rn = e_rn['roll_number']
-            # check roll number
-            if roll_number == existing_rn:
-                print('\nThis roll number', roll_number, 'is already exists. Pleas try a new.')
-                # return get_roll_number
-                print('\nNaye student ka registration uniqe roll number ke sath kare:\n')
-                register_new_student(school_name, class_name)
+            # print('Type: ', type(json.loads(r)))
+            all_rn_as_dict = json.loads(r)
+            get_single_roll_number = all_rn_as_dict['roll_number']
+            # print('\nRoll number: ', get_single_roll_number)
+
+            # ab existing roll number me se new roll number ko check karn ahai using condition
+            if roll_number == get_single_roll_number:
+                message = print('\nYe roll number => ', roll_number ,' <= pahle se hai. New roll number try karen.')
+                # register_new_student(school_name, class_name)
+                return message
+
+        # return print('\nTHis is studnets roll number: ', roll_number)        
 
 # for registering new stuent
 def register_new_student(school_name, class_name):
-    try:
-        roll_number = int(input('\nEnter your roll number: '))
-        check_roll_number(school_name, class_name, roll_number)
+    try:        
+        # roll number ko chekc karne ke liye sab se pahle entered roll number ko function ko pass karna hai.
+        roll_number = int(input('\nEnter student roll number: '))
+        
+        recive_message = check_roll_number(school_name, class_name, roll_number)
         # print(recive_result)
         # exit()
         
